26d23d2fd9b0a3c4120c5103cffd297f57f02457
Yousef Mahrous <yousefmahrous@YM-MBP.local>
2024-07-11 21:33:38 +0300

    pass 3rd stage

--- app/main.js
+++ app/main.js
@@ -5,7 +5,13 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    socket.on('data', (data) => {
+        const request = data.toString();
+        if(request.startsWith("Get / ")) {
+            socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        }
+        socket.write("HTTP/1.1 404 NOT FOUND\r\n\r\n");
+    })
     socket.on("close", () => {
         socket.end();
     });
