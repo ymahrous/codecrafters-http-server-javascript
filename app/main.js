89ade2015ce6efc8ba91042eec355031e35c3167
Yousef Mahrous <yousefmahrous@YM-MBP.local>
2024-07-12 13:20:59 +0300

    pass 3rd stage

--- app/main.js
+++ app/main.js
@@ -8,11 +8,20 @@ const server = net.createServer((socket) => {
     socket.on('data', (data) => {
         const request = data.toString();
         const [method, path] = request.split(" ");
-        if (method === "GET" && path === "/") {
+        const url = request.split(' ')[1];
+        if(url == "/"){
             socket.write("HTTP/1.1 200 OK\r\n\r\n");
-        } else {
+        } else if(url.includes("/echo/")){
+            const content = url.split('/echo/')[1];
+            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`);
+        }else{
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
         }
+        // if (method === "GET" && path === "/") {
+        //     socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        // } else {
+        //     socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        // }
     })
     socket.on("close", () => {
         socket.end();
