1e2f3bf8bf7cecb5dc59cc4d38cf532fdfc59056
Yousef Mahrous <yousefmahrous@YM-MBP.local>
2024-07-13 16:25:52 +0300

    pass 5th stage

--- app/main.js
+++ app/main.js
@@ -6,11 +6,15 @@ const server = net.createServer((socket) => {
         const request = data.toString();
         // const [method, path] = request.split(" ");
         const url = request.split(' ')[1];
+        const headers = request.split('\r\n');
         if(url == "/"){
             socket.write("HTTP/1.1 200 OK\r\n\r\n");
         } else if(url.includes("/echo/")){
             const content = url.split('/echo/')[1];
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`);
+        } else if(url == '/user-agent') {
+            const userAgent = headers[2].split('User-Agent: ')[1];
+            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`);
         } else {
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
         }
