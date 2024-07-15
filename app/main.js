cbadf6db7f3d07061287f2ff6d05dbe36dfde764
Yousef Mahrous <yousefmahrous@YM-MBP.local>
2024-07-15 22:04:24 +0300

    pass 7th stage

--- app/main.js
+++ app/main.js
@@ -27,14 +27,6 @@ const server = net.createServer((socket) => {
         } else if(url == '/user-agent') {
             const userAgent = headers[2].split('User-Agent: ')[1];
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`);
-        }  else if(url.includes("/files/") && data.toString().split(" ")[0] === "GET"){
-            const fileName = url.split('/files/')[1];
-            fs.readFile(`/tmp/data/codecrafters.io/http-server-tester/${fileName}`, (err, data) => {
-                if(err) {
-                    socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-                }
-                socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${data.length}\r\n\r\n${data}`);
-            });
         } else if(url.includes("/files/") && data.toString().split(" ")[0] === "POST") {
             let fileName = url.split('/')[2];
             console.log(`filename:${fileName}`);
@@ -44,6 +36,14 @@ const server = net.createServer((socket) => {
             fs.writeFileSync(filePath, file);
 
             socket.write("HTTP/1.1 201 CREATED\r\n\r\n");
+        } else if(url.includes("/files/")){
+            const fileName = url.split('/files/')[1];
+            fs.readFile(`/tmp/data/codecrafters.io/http-server-tester/${fileName}`, (err, data) => {
+                if(err) {
+                    socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+                }
+                socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${data.length}\r\n\r\n${data}`);
+            });
         } else {
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
         }
