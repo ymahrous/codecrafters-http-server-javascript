3cb5315c705cee025f683b5a59b23c5902f63df7
Yousef Mahrous <yousefmahrous@YM-MBP.local>
2024-07-15 21:59:23 +0300

    pass 7th stage

--- app/main.js
+++ app/main.js
@@ -36,7 +36,8 @@ const server = net.createServer((socket) => {
                 socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${data.length}\r\n\r\n${data}`);
             });
         } else if(url.includes("/files/") && data.toString().split(" ")[0] === "POST") {
-            const fileName = url.split('/files/')[2];
+            let fileName = url.split('/')[2];
+            console.log(`filename:${fileName}`);
             const filePath = FILES_DIR + fileName;
             const file = fileName.toString("utf-8").split("\r\n\r\n")[1];
 
