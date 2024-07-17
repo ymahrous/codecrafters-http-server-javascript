fe1e3867448de43a861c669ff0fdb0e3ba614a06
Yousef Mahrous <yousefmahrous@YM-MBP.local>
2024-07-17 21:36:56 +0300

    pass 8th stage

--- app/main.js
+++ app/main.js
@@ -50,7 +50,7 @@ const server = net.createServer((socket) => {
         } else if(url.includes("/echo/")){
             const content = url.split('/echo/')[1];
             if(headers["Accept-Encoding"] == "gzip") {
-                socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n`);
+                socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}\r\n\r\nContent-Encoding: gzip`);
             }
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`);
 
