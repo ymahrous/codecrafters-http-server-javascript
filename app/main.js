7d4378de5db0e12907415e865f6611fe21bc9f00
Yousef Mahrous <yousefmahrous@YM-MBP.local>
2024-07-22 13:11:39 +0300

    pass 8th stage

--- app/main.js
+++ app/main.js
@@ -7,11 +7,12 @@ const server = net.createServer((socket) => {
         const request = data.toString().split("\r\n");
         const url = data.toString().split(' ')[1];
         const [method, path, protocol] = request[0].split(" ");
+
         const headers = {};
         request.slice(1).forEach((header) => {
             const [key, value] = header.split(" ");
             if (key && value) {
-            headers[key] = value;
+                headers[key] = value;
             }
         });
 
@@ -48,7 +49,6 @@ const server = net.createServer((socket) => {
             const body = req[req.length - 1];
             fs.writeFileSync(filename, body);
             socket.write(`HTTP/1.1 201 Created\r\n\r\n`);
-
         } else {
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
         }
